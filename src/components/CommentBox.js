627ec777620a580a5db6e4a14d03d5ece8c71f69
rajan <rajangreedge@gmail.com>
2019-05-07 19:00:06 +0530

    used shallow enzyme

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default ()=> {
-    return  <div>CoomentBox</div>
+    return  <div>Comment Box</div>
 }
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+
+it('shows a comment box', ()=> {
+    const div = document.createElement('div');
+    
+/*     ReactDOM.render(<App/>, div);
+
+    //Looks inside the div
+    // and checks to see if the commentBox is in there
+    
+    //clean up the div
+    //clear memory to increase performance.
+    //look for the App component inside div and remove it completely.
+    //console.log(div.innerHTML);
+
+    expect(div.innerHTML).toContain('Comment Box');
+    ReactDOM.unmountComponentAtNode(div); */
+
+    //using shallow
+
+    const wrapped = shallow(<App/>);//return a component with new function on it.that a new wrapped component.
+    
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
