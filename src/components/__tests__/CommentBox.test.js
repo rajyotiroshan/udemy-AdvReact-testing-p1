3f93e099dc2d37086bd53465dfda87c880f2e87b
rajan <rajangreedge@gmail.com>
2019-05-08 10:46:23 +0530

    CommentBox test "has text area that user can typed in"

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,13 +16,29 @@ afterEach(()=>{
 });
 
 it('has a textarea and a button', ()=> {
-    wrapped = mount(<CommentBox />);
 
     //console.log(wrapped.find('textarea'));
     //console.log(wrapped.find('button'));
 
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+
+it(' has a textarea that user can typed in', ()=>{
+
+    wrapped.find('textarea')//1. ref to textarea.
+    .simulate('change', {//2. simulate a change event
+        target: {value: 'new Comment'} //3.fake event object:: merge with the event object passed to the handler.
+    }); 
+    
+    wrapped.update();//4.force update the
+    
+    expect(
+        wrapped.find('textarea')//find textarea ref
+        .prop('value')//access it's value prop
+        ).toEqual('new Comment');
+    
 });
 
 
