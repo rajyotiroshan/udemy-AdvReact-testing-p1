11af75996f10768d4be9a1ac78c6aa3fccb23ffb
rajan <rajangreedge@gmail.com>
2019-05-09 11:04:12 +0530

    render comments from redux store inside Commentlist compoenent

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux'; 
 
-export default ()=> {
-    return <div>CommentLsit</div>
-};
+class CommentList extends React.Component {
+
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <ul>
+                   {this.renderComments()} 
+                </ul>
+                
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        comments: state.comments
+    }
+}
+
+export default connect(mapStateToProps)(CommentList);
