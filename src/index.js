ab00e2b6f8fa65bd0fdf860879023d8adcd5421f
rajan <rajangreedge@gmail.com>
2019-05-11 09:53:52 +0530

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import Root from 'Root';
 
 ReactDOM.render(
     <Root>
-        <App /> {/** passed as a value props.children inside Root component*/}
+        <BrowserRouter>
+            <Route path="/" component={App}/>
+        </BrowserRouter> {/** passed as a value props.children inside Root component*/}
     </Root>, 
     document.getElementById('root'));
 
