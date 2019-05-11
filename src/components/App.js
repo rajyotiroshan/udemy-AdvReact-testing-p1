ee5f680248c545009fbd3d545663cc66e25903f4
rajan <rajangreedge@gmail.com>
2019-05-11 10:21:58 +0530

    header and connect to redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-    return (
-        <div>
-            <Route path="/post" component={CommentBox}/>
-            <Route path="/" exact component={CommentList}/>
-        </div>
-    );
+class App extends React.Component  {
+    
+    renderButton(){
+        if(this.props.auth) {
+            return (
+                <button>Sign Out</button>
+            )
+        }else {
+            return <button>Sign In</button>
+        }
+    }
+
+    renderHeading() {
+        return (
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/post">Post A Comment</Link>
+                </li>
+                <li>
+                   {this.renderButton()}
+                </li>
+            </ul>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderHeading()}
+                <Route path="/post" component={CommentBox}/>
+                <Route path="/" exact component={CommentList}/>
+            </div>
+        );        
+    }
+
 };
+
+const mapStateToProps = (state)=> {
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps)(App);
